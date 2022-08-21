ec3e3baf571b8fc11b2b106274f3dfdec031e9c0
Davi <daviiemanuel2000@gmail.com>
2022-08-21 14:31:31 -0300

    Home component creation

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -3,14 +3,13 @@ import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
+import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
 
 export default props =>
     <div className="app">
         <Logo />
         <Nav icon="home" icon2="users"/>
-        <Main icon="home" title="Início"
-            subtitle="Segundo projeto do capítulo de React." />
+        <Home />
         <Footer />
     </div>
